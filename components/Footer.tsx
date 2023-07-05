8399fda6676a665b837644957d7a4ee7389bda32
Godfrey Lebo <emorylebo@gmail.com>
2023-07-05 20:47:46 +0100

    ADD Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,29 @@
 import React from 'react'
 
+
+const navigation = {
+    solutions: [
+      { name: 'Marketing', href: '#' },
+      { name: 'Analytics', href: '#' },
+      { name: 'Insights', href: '#' },
+    ],
+    support: [
+      { name: 'Pricing', href: '#' },
+      { name: 'Documentation', href: '#' },
+      { name: 'Guides', href: '#' },
+    ],
+    company: [
+      { name: 'About', href: '#' },
+      { name: 'Jobs', href: '#' },
+      { name: 'Partners', href: '#' },
+    ],
+    legal: [
+      { name: 'Claim', href: '#' },
+      { name: 'Privacy', href: '#' },
+      { name: 'Terms', href: '#' },
+    ],
+  }
+
 function Footer() {
   return (
     <div>Footer</div>
