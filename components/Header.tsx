922349214b0861af557af8bcffafdcb9695767f1
Godfrey Lebo <emorylebo@gmail.com>
2023-07-05 08:59:25 +0100

    UPDATE Banner

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,24 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import { Popover, Transition } from '@headlessui/react'
+
+import {
+    BookmarkAltIcon,
+    CalendarIcon,
+    ChartBarIcon,
+    CursorClickIcon,
+    MenuIcon,
+    PhoneIcon,
+    PlayIcon,
+    QuestionMarkCircleIcon,
+    RefreshIcon,
+    ShieldCheckIcon,
+    SupportIcon,
+    ViewGridIcon,
+    HomeIcon,
+    NewspaperIcon,
+    XIcon,
+  } from '@heroicons/react/outline'
+  import { ChevronDownIcon } from '@heroicons/react/solid'
 
 export default function Header() {
   return (
