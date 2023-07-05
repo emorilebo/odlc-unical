3dad05088c49cf59a4daa7d627662f939e6251f8
Godfrey Lebo <emorylebo@gmail.com>
2023-07-05 19:02:29 +0100

    UPDATE team

--- components/Team.tsx
+++ components/Team.tsx
@@ -1,30 +1,27 @@
-import React from 'react'
-
+import React from "react";
 
 const people = [
-    {
-      name: 'Adedayo Otusanya',
-      role: 'Partner',
-      imageUrl: '/adetayo.jpg',
-      bio: 'Adetayo is a lawyer by profession with in-depth experience in the real estate industry. After graduating from Law School in 2010, Adedayo started the Elisio Law Office. In 2012, he started Elisio Properties with the aim of carrying out strategic investment in real state and property consultancy in Lagos, Nigeria. He has immense experience in the real estate value chain across Nigeria and the United States, with notable successes. Adedayo believes in providing the best deal to the client by understanding their requirement.',
-      twitterUrl: '#',
-      linkedinUrl: '#',
-    },
-    {
-      name: 'Usman Salawu',
-      role: 'Partner',
-      imageUrl: '/usman.jpg',
-      bio: 'Usman has a BSc in Geology and an MSc in Applied Computing and Information Technology. His wealth of experience cuts across logistics, telecoms, oil and gas, automobile and real estate in Nigeria and the U.S. Usman possesses in-depth knowledge of the real estate market in Atlanta.Usman is a co-founder of Elisio Properties and the founder of other successful businesses.',
-      twitterUrl: '#',
-      linkedinUrl: '#',
-    },
-    // More people...
-  ]
+  {
+    name: "Professor Florence Banku Obi  FAFTRA,  FNAEC  MNAE  JP",
+    role: "Vice Chancellor",
+    imageUrl: "/adetayo.jpg",
+    bio: "B. Ed. (Jos), M. Ed. (Jos), PGCEE (Glasgow), Ph.D. (Calabar).",
+    twitterUrl: "#",
+    linkedinUrl: "#",
+  },
+  {
+    name: "Prof. Mbe Egom Nja  FRSS (London), JP",
+    role: "Director",
+    imageUrl: "/usman.jpg",
+    bio: "B. Sc. (Jos), M. Sc. (Calabar), Ph.D. (Ibadan), ",
+    twitterUrl: "#",
+    linkedinUrl: "#",
+  },
+  // More people...
+];
 
 function Team() {
-  return (
-    <div>Team</div>
-  )
+  return <div>Team</div>;
 }
 
-export default Team
+export default Team;
