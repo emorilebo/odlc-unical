8328c94c29643d89acd0a0d066c15840a7c3b3e9
Godfrey Lebo <emorylebo@gmail.com>
2023-07-20 11:53:13 +0100

    UPDATE odlcform date of birtj

--- pages/odlcform.tsx
+++ pages/odlcform.tsx
@@ -111,6 +111,27 @@ export default function Odlcform() {
             </div>
           </div>
           <div className="sm:col-span-2">
+            <label htmlFor="phone-number" className="block text-sm font-semibold leading-6 text-gray-900">
+              Date of Birth
+            </label>
+            <div className="relative mt-2.5">
+              <div className="absolute inset-y-0 left-0 flex items-center">
+                <label htmlFor="country" className="sr-only">
+                DOB
+                </label>
+                
+                
+              </div>
+              <input
+                type="date"
+                name="phone-number"
+                id="phone-number"
+                autoComplete="tel"
+                className="block w-full rounded-md border-0 px-3.5 py-2 pl-20 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+          <div className="sm:col-span-2">
             <label htmlFor="company" className="block text-sm font-semibold leading-6 text-gray-900">
               State
             </label>
