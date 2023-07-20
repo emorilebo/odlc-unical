0c5a9f1228a159b9ce521bdc0b950e0b4f02555d
Godfrey Lebo <emorylebo@gmail.com>
2023-07-20 11:39:53 +0100

    UPDATE odlcform

--- pages/odlcform.tsx
+++ pages/odlcform.tsx
@@ -7,6 +7,19 @@ import { Switch } from '@headlessui/react'
 // }
 
 
+const olevels = [
+  { name: '1', title: '', email: '', role: '' },
+  { name: '2', title: '', email: '', role: '' },
+  { name: '3', title: '', email: '', role: '' },
+  { name: '4', title: '', email: '', role: '' },
+  { name: '5', title: '', email: '', role: '' },
+  { name: '6', title: '', email: '', role: '' },
+  { name: '7', title: '', email: '', role: '' },
+  { name: '8', title: '', email: '', role: '' },
+  { name: '9', title: '', email: '', role: '' },
+  // More people...
+]
+
 
 export default function Odlcform() {
   const [agreed, setAgreed] = useState(false)
@@ -141,53 +154,71 @@ export default function Odlcform() {
           </div>
           <div className="sm:col-span-2">
             <label htmlFor="message" className="block text-sm font-semibold leading-6 text-gray-900">
-              Message
+              OLevel
             </label>
-            <div className="mt-2.5">
-              <textarea
-                name="message"
-                id="message"
-                rows={4}
-                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                defaultValue={''}
-              />
-            </div>
-          </div>
-          <Switch.Group as="div" className="flex gap-x-4 sm:col-span-2">
-            <div className="flex h-6 items-center">
-              <Switch
-                checked={agreed}
-                onChange={setAgreed}
-                // className={classNames(
-                //   agreed ? 'bg-indigo-600' : 'bg-gray-200',
-                //   'flex w-8 flex-none cursor-pointer rounded-full p-px ring-1 ring-inset ring-gray-900/5 transition-colors duration-200 ease-in-out focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
-                // )}
+            <div className="px-4 sm:px-6 lg:px-8">
+      <div className="sm:flex sm:items-center">
+        <div className="sm:flex-auto">
+          <h1 className="text-base font-semibold leading-6 text-gray-900">First sitting</h1>
+        </div>
+        
+      </div>
+      <div className="-mx-4 mt-8 sm:-mx-0">
+        <table className="min-w-full divide-y divide-gray-300">
+          <thead>
+            <tr>
+              <th scope="col" className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0">
+                Number
+              </th>
+              <th
+                scope="col"
+                className="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 sm:table-cell"
               >
-                <span className="sr-only">Agree to policies</span>
-                <span
-                  aria-hidden="true"
-                  // className={classNames(
-                  //   agreed ? 'translate-x-3.5' : 'translate-x-0',
-                  //   'h-4 w-4 transform rounded-full bg-white shadow-sm ring-1 ring-gray-900/5 transition duration-200 ease-in-out'
-                  // )}
-                />
-              </Switch>
-            </div>
-            <Switch.Label className="text-sm leading-6 text-gray-600">
-              By selecting this, you agree to our{' '}
-              <a href="#" className="font-semibold text-indigo-600">
-                privacy&nbsp;policy
-              </a>
-              .
-            </Switch.Label>
-          </Switch.Group>
+                Subject
+              </th>
+              <th
+                scope="col"
+                className="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 lg:table-cell"
+              >
+                Grade
+              </th>
+              
+              
+            </tr>
+          </thead>
+          <tbody className="divide-y divide-gray-200 bg-white">
+            {olevels.map((olevel) => (
+              <tr key={olevel.email}>
+                <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">
+                  {olevel.name}
+                </td>
+                <td className="hidden whitespace-nowrap px-3 py-4 text-sm text-gray-500 sm:table-cell">
+                  {olevel.title}
+                </td>
+                <td className="hidden whitespace-nowrap px-3 py-4 text-sm text-gray-500 lg:table-cell">
+                  {olevel.email}
+                </td>
+                <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{olevel.role}</td>
+                <td className="whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
+                  <a href="#" className="text-indigo-600 hover:text-indigo-900">
+                    Edit<span className="sr-only">, {olevel.name}</span>
+                  </a>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+          </div>
+          
         </div>
         <div className="mt-10">
           <button
             type="submit"
-            className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            className="block w-full rounded-md bg-red-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
-            Lets talk
+            Proceed
           </button>
         </div>
       </form>
