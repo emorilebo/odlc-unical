bb6f1be938d931add425bf04609fa60e9e800b36
Godfrey Lebo <emorylebo@gmail.com>
2023-10-09 17:09:11 +0100

    UPDATE

---
+++ components/Odlcform.tsx
@@ -0,0 +1,381 @@
+import { useLayoutEffect, useRef, useState } from "react";
+import { ChevronDownIcon } from "@heroicons/react/solid";
+import { Switch } from "@headlessui/react";
+import Link from "next/link";
+
+function classNames(...classes: any[]) {
+  return classes.filter(Boolean).join(" ");
+}
+
+const olevels = [
+  { name: "1", title: "", email: "", role: "" },
+  { name: "2", title: "", email: "", role: "" },
+  { name: "3", title: "", email: "", role: "" },
+  { name: "4", title: "", email: "", role: "" },
+  { name: "5", title: "", email: "", role: "" },
+  { name: "6", title: "", email: "", role: "" },
+  { name: "7", title: "", email: "", role: "" },
+  { name: "8", title: "", email: "", role: "" },
+  { name: "9", title: "", email: "", role: "" },
+];
+
+const people = [
+  {
+    name: "Lindsay Walton",
+    title: "Front-end Developer",
+    email: "lindsay.walton@example.com",
+    role: "Member",
+  },
+  // More people...
+];
+
+function Odlcform() {
+    const [agreed, setAgreed] = useState(false);
+
+  const checkbox = useRef();
+  const [checked, setChecked] = useState(false);
+  const [indeterminate, setIndeterminate] = useState(false);
+  const [selectedPeople, setSelectedPeople] = useState([]);
+    return (
+        <div className="isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
+          <div
+            className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]"
+            aria-hidden="true"
+          >
+            <div
+              className="relative left-1/2 -z-10 aspect-[1155/678] w-[36.125rem] max-w-none -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-40rem)] sm:w-[72.1875rem]"
+              style={{
+                clipPath:
+                  "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+              }}
+            />
+          </div>
+          <div className="mx-auto max-w-2xl ">
+            <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+              Admission Requirements
+            </h2>
+            <p className="mt-2 text-lg leading-8 text-gray-600">
+              <li className="font-semibold mt-6">
+                {" "}
+                Admission Requirements for Candidates seeking Admission into Year 1
+                Candidates seeking admission to Year 1 should possess one of the
+                following:
+              </li>
+              <p>
+                i. Five credits passes in the SSCE/GCE/NECO/NABTEB OLevel, including
+                English Language and Mathematics, and any other three subjects
+                relevant to their cognate area taken at not more than two sittings.
+              </p>
+              <p className="mb-4">
+                ii. Five merits in the NABTEB/TC II in relevant subjects.
+              </p>
+              <li className="font-semibold">
+                Admission Requirements for Candidates seeking Admission into Year 2
+                Candidates seeking admission to Year 2 should possess one of the
+                following:
+              </li>
+              <p>
+                1. The National Certificate of Education (NCE) with at least Merit
+                grades in two relevant teaching subjects.
+              </p>
+              <p>
+                2. The National Diploma (ND) in relevant fields with a minimum of
+                lower credit.
+              </p>
+              <p className="mb-4">
+                3. GCE ALevel with at least credit grades in three relevant
+                subjects.
+              </p>
+              <li className="font-semibold">
+                Admission Requirements for Candidates seeking Admission into Year 3
+                Candidates seeking admission to Year 3 in the relevant programme
+                should possess at least one of the followings:
+              </li>
+              <p>
+                1. Higher National Diploma (HND) in relevant fields with a minimum
+                overall grade of lower credit.
+              </p>
+              <p>
+                2. Bachelors degree with a minimum Pass Class in a relevant
+                discipline obtained from a recognized university.
+              </p>
+              <p className="mb-4">
+                3. Recognized professional qualifications such as ACA, ACMA, ACIA,
+                ACIS, ACIB, etc.
+              </p><p className="mb-4">
+                3. Recognized professional qualifications such as ACA, ACMA, ACIA,
+                ACIS, ACIB, etc.
+              </p>
+              Candidates seeking admission to Years 2 and 3 must also possess the
+              minimum OLevel requirements.
+            </p>
+            <p className="mt-10">
+                If you meet the requirements above, click on the Proceed button below to acquire the admission form for N20, 000.
+              </p>
+          </div>
+          {/* <form
+            action="#"
+            method="POST"
+            className="mx-auto mt-10 max-w-xl sm:mt-12"
+          >
+            <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+              <div>
+                <label
+                  htmlFor="first-name"
+                  className="block text-sm font-semibold leading-6 text-gray-900"
+                >
+                  Surname
+                </label>
+                <div className="mt-2.5">
+                  <input
+                    type="text"
+                    name="first-name"
+                    id="first-name"
+                    autoComplete="given-name"
+                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+              <div>
+                <label
+                  htmlFor="last-name"
+                  className="block text-sm font-semibold leading-6 text-gray-900"
+                >
+                  First name
+                </label>
+                <div className="mt-2.5">
+                  <input
+                    type="text"
+                    name="last-name"
+                    id="last-name"
+                    autoComplete="family-name"
+                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+    
+              <div className="sm:col-span-2">
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-semibold leading-6 text-gray-900"
+                >
+                  Email
+                </label>
+                <div className="mt-2.5">
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    autoComplete="email"
+                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+              <div className="sm:col-span-2">
+                <label
+                  htmlFor="phone-number"
+                  className="block text-sm font-semibold leading-6 text-gray-900"
+                >
+                  Phone number
+                </label>
+                <div className="relative mt-2.5">
+                  <div className="absolute inset-y-0 left-0 flex items-center">
+                    <label htmlFor="country" className="sr-only">
+                      Country
+                    </label>
+                  </div>
+                  <input
+                    type="tel"
+                    name="phone-number"
+                    id="phone-number"
+                    autoComplete="tel"
+                    className="block w-full rounded-md border-0 px-3.5 py-2 pl-20 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+              <div className="sm:col-span-2">
+                <label
+                  htmlFor="phone-number"
+                  className="block text-sm font-semibold leading-6 text-gray-900"
+                >
+                  Date of Birth
+                </label>
+                <div className="relative mt-2.5">
+                  <div className="absolute inset-y-0 left-0 flex items-center">
+                    <label htmlFor="country" className="sr-only">
+                      DOB
+                    </label>
+                  </div>
+                  <input
+                    type="date"
+                    name="phone-number"
+                    id="phone-number"
+                    autoComplete="tel"
+                    className="block w-full rounded-md border-0 px-3.5 py-2 pl-20 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+              <div className="sm:col-span-2">
+                <label
+                  htmlFor="phone-number"
+                  className="block text-sm font-semibold leading-6 text-gray-900"
+                >
+                  Gender
+                </label>
+                <div className="flex mt-2.5">
+                  <div className=" inset-y-0 left-0 items-center space-x-4">
+                    <label className="space-x-2">
+                      <input type="checkbox" />
+                      <span>Male</span>
+                    </label>
+                    <label className="space-x-2">
+                      <input type="checkbox" />
+                      <span>Female</span>
+                    </label>
+                  </div>
+                </div>
+              </div>
+              <div className="sm:col-span-2">
+                <label
+                  htmlFor="company"
+                  className="block text-sm font-semibold leading-6 text-gray-900"
+                >
+                  State
+                </label>
+                <div className="mt-2.5">
+                  <input
+                    type="text"
+                    name="company"
+                    id="company"
+                    autoComplete="organization"
+                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+              <div className="sm:col-span-2">
+                <label
+                  htmlFor="company"
+                  className="block text-sm font-semibold leading-6 text-gray-900"
+                >
+                  L.G.A
+                </label>
+                <div className="mt-2.5">
+                  <input
+                    type="text"
+                    name="company"
+                    id="company"
+                    autoComplete="organization"
+                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+              <div className="sm:col-span-2">
+                <label
+                  htmlFor="company"
+                  className="block text-sm font-semibold leading-6 text-gray-900"
+                >
+                  Course
+                </label>
+                <div className="mt-2.5">
+                  <input
+                    type="text"
+                    name="company"
+                    id="company"
+                    autoComplete="organization"
+                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+              <div className="sm:col-span-2">
+                <label
+                  htmlFor="message"
+                  className="block text-sm font-semibold leading-6 text-gray-900"
+                >
+                  OLevel
+                </label>
+                <div className="px-4 sm:px-6 lg:px-8">
+                  <div className="sm:flex sm:items-center">
+                    <div className="sm:flex-auto">
+                      <h1 className="text-base font-semibold leading-6 text-gray-900">
+                        First sitting
+                      </h1>
+                    </div>
+                  </div>
+                  <div className="-mx-4 mt-8 sm:-mx-0">
+                    <table className="min-w-full divide-y divide-gray-300">
+                      <thead>
+                        <tr>
+                          <th
+                            scope="col"
+                            className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0"
+                          >
+                            Number
+                          </th>
+                          <th
+                            scope="col"
+                            className="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 sm:table-cell"
+                          >
+                            Subject
+                          </th>
+                          <th
+                            scope="col"
+                            className="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 lg:table-cell"
+                          >
+                            Grade
+                          </th>
+                        </tr>
+                      </thead>
+                      <tbody className="divide-y divide-gray-200 bg-white">
+                        {olevels.map((olevel) => (
+                          <tr key={olevel.email}>
+                            <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">
+                              {olevel.name}
+                            </td>
+                            <td className="hidden whitespace-nowrap px-3 py-4 text-sm text-gray-500 sm:table-cell">
+                              {olevel.title}
+                            </td>
+                            <td className="hidden whitespace-nowrap px-3 py-4 text-sm text-gray-500 lg:table-cell">
+                              {olevel.email}
+                            </td>
+                            <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                              {olevel.role}
+                            </td>
+                            <td className="whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
+                              <a
+                                href="#"
+                                className="text-indigo-600 hover:text-indigo-900"
+                              >
+                                Edit<span className="sr-only">, {olevel.name}</span>
+                              </a>
+                            </td>
+                          </tr>
+                        ))}
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="mt-10">
+              <button
+                type="submit"
+                className="block w-full rounded-md bg-red-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Proceed
+              </button>
+            </div>
+          </form> */}
+          
+            <div className="mt-10 mx-auto max-w-xl sm:mt-12">
+                      <Link
+                        href="https://odlcportal.unical.edu.ng/Identity/Admission/SearchAdmissionListWithPin"
+                        className="flex w-full items-center justify-center rounded-md border border-transparent bg-red-600 px-8 py-3 text-md font-medium text-white hover:bg-red-400 md:py-4 md:px-10 md:text-md"
+                      >
+                        Proceed
+                      </Link>
+                    </div>
+        </div>
+      );
+}
+
+export default Odlcform
